b7cb4dee26629a3b13fb1d5d5dcff16d7792fdbf
AnshuShetty <115566250+AnshuShetty@users.noreply.github.com>
2023-07-23 11:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('navbar li a');
-window.onscroll=()=>{
-  sections.forEach(sec =>{
-    let top = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-  });
-};
- 
-function ValidateEmail(email) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
 
-const email = document.getElementById('exampleInputEmail');
-const feedback = document.getElementById('Feedback')
-const form = document.getElementById('Feedbackform')
-const errormssg = document.getElementById('error')
-form.addEventListener('submit', (e) => {
-  let messages=[]
-  if(feedback.value.length<=4){
-    messages.push('wirte atleast 5 words')
+function ValidateEmail(exampleInputEmail) 
+{
+  var email = document.getElementById('exampleInputEmail');
+  var emailregex= /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;;
+
+ if(!emailregex.test(email))
+  {
+    alert("You have entered an invalid email address!");
+    return false;
   }
-  if(messages.length>=0){
-    e.preventDefault()
-    errormssg.innerText = messages.join(', ');
+  else{
+
+    alert("Valid email id");
+    return true;
   }
-})
+
+}
+function mssgcheck(){
+  var mssg = document.getElementById('Feedback');
+  if(!mssg<5){
+    alert("Write atleast one Message!")
+  }
+}
+// const email = document.getElementById('exampleInputEmail');
+// const feedback = document.getElementById('Feedback')
+// const form = document.getElementById('Feedbackform')
+// const errormssg = document.getElementById('error')
+// form.addEventListener('submit', (e) => {
+//   let messages=[]
+//   if(feedback.value.length<=4){
+//     messages.push('wirte atleast 5 words')
+//   }
+//   if(messages.length>=0){
+//     e.preventDefault()
+//     errormssg.innerText = messages.push('hi');
+//   }
+// })
+
