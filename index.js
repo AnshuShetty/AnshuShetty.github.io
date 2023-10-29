f57930d7911e1f33e3639d9dd0597798cf779335
AnshuShetty <115566250+AnshuShetty@users.noreply.github.com>
2023-10-29 17:07:02 +0530

    new  changes Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function validateEmail() {
   var email = document.getElementById('exampleInputEmail').value;
-  //var emailregex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  var emailregex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
-
+  // var emailregex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var emailregex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  // return emailregex.test(email);
   if (!emailregex.test(email)) {
     alert('Invalid email!');
     return false;
@@ -11,7 +11,6 @@ function validateEmail() {
   }
 }
 const linkElement = document.getElementById("sbtn");
-
 linkElement.addEventListener("click", redirectToOtherPartOfPage);
 
 function redirectToOtherPartOfPage() {
@@ -32,30 +31,39 @@ function checkWordCount() {
 
   // Count the number of words.
   var wordCount = words.length;
-
-  // If the word count is less than 5, alert the user.
-  if (wordCount < 5) {
-    alert("Write atleast 10 words");
+  
+  // If the word count is less than 2, alert the user.
+  if (wordCount < 2) {
+    alert("Write atleast 3 words");
     console.log("write");
   }
+  
 }
 
 const formElement = document.getElementById("Feedbackform");
 const messageElement = document.getElementById("message");
-formElement.addEventListener("submit", (event) => {
-  alert("do you want to send ");
 
-  // Prevent the form from submitting.
-  event.preventDefault();
+formElement.addEventListener("submit", (event) => {
+  // Prompt the user to confirm whether they want to send the feedback.
+  const confirm = window.confirm("Do you want to send this feedback?");
+
+  // Prevent the form from submitting if the user clicks **No**.
+  if (!confirm) {
+    event.preventDefault();
+    return;
+  }
 
   // Display the message that the form has been submitted.
   messageElement.innerHTML = "Message sent!";
+
+  // Reset the form.
   document.getElementById('Feedbackform').reset();
 });
 
+
 function downloadPDF() {
   // Get the path of the PDF file.
-  const pdfPath = "Images/Anshu_Resume.pdf";
+  const pdfPath = "Images/Anshu%20_Resume.pdf";
 
   // Create a new anchor element.
   const anchor = document.createElement("a");
@@ -69,24 +77,21 @@ function downloadPDF() {
   // Click the anchor element to download the PDF file.
   anchor.click();
 }
-// function storeMessage(email, message) {
-//   var spreadsheetId = 'AKfycbxR7LFNxFLJSCUqijh2DdOfn57uJ3KFFJMWALUkTZshdzm0uuo3zXyAYTN_cLYd7R0jDQ';
-//   var range = 'Sheet1!A:B';
-//   var values = [[email, message]];
+function storeMessage(email, message) {
+  var spreadsheetId = 'AKfycbxR7LFNxFLJSCUqijh2DdOfn57uJ3KFFJMWALUkTZshdzm0uuo3zXyAYTN_cLYd7R0jDQ';
+  var range = 'Sheet1!A:B';
+  var values = [[email, message]];
 
-//   var request = gapi.client.sheets.spreadsheets.values.append({
-//     spreadsheetId: spreadsheetId,
-//     range: range,
-//     valueInputOption: 'USER_ENTERED',
-//     resource: {
-//       values: values,
-//     },
-//   });
+  var request = gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: spreadsheetId,
+    range: range,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: values,
+    },
+  });
 
-//   request.execute(function(response) {
-//     console.log('Message stored successfully.');
-//   });
-// }
-
-
-
+  request.execute(function(response) {
+    console.log('Message stored successfully.');
+  });
+}
